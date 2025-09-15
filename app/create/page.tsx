819a94b9f0d3cf2ad8ee904657acb5b3e008c648
Ron <imrondaramae@gmail.com>
2025-09-15 13:19:29 +0700

    Update Fix

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -100,6 +100,13 @@ const Create = () => {
           >
             Submit
           </button>
+          <button
+            type="button"
+            onClick={() => router.push('/')}
+            className="inline-flex justify-center py-2 px-4 m-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+          >
+            Back
+          </button>
         </div>
       </form>
     </div>
--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -120,6 +120,13 @@ const Edit = ({ params }: { params: { id: string } }) => {
           >
             Update
           </button>
+          <button
+            type="button"
+            onClick={() => router.push('/')}
+            className="inline-flex justify-center py-2 px-4 m-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+          >
+            Back
+          </button>
         </div>
       </form>
     </div>
